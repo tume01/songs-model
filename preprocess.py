9112043642985c8d898189379d90cb24a60922c2
Franco Tume <tume@Tektons-MacBook-Pro.local>
2017-06-03 20:06:34 -0500

    first commit

---
+++ preprocess.py
@@ -0,0 +1,63 @@
+import nltk
+import csv
+import string
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import json
+from collections import Counter
+
+wordnet_lemmatizer = WordNetLemmatizer()
+translator = str.maketrans('', '', string.punctuation)
+T = {}
+T['love'] = ['love','romance','romantic','love song','soulmate','relationship','relationships','marriage','crush','love triangle','sweet','young love','true love']
+T['sad'] = ['lonely','depression','sad','loneliness','sadness','dark','broken','alone','emo','darkness','isolation','cry','homesick']
+T['breakup'] = ['breakup','break up','break-up','heartbreak','cheating','betrayal','divorce','infidelity','disillusionment','betrayal','disappointment','guilt','heartache','jealousy','leaving','unrequited love']
+T['nostalgia'] = ['melancholia','loss','melancholy','memories','regret','nostalgia','homesick','longing']
+T['death'] = ['suicidal','suicide','death','war','murder','abuse','pain','evil','funeral','drugs','addiction','alcoholism','heroin','bullying','satan','domestic abuse','cancer','cocaine','weed','dying']
+T['sex'] = ['sex','rape','sexy','oral sex','masturbation','sexuality','prostitution','lust','virginity','desire','voyeurism']
+T['happy'] = ['happysongs','happy','happiness','beauty','comedy','inspiration','dream','empathy','life','christmas','holiday','hope','faith','friendship','beautiful','amazing','freedom','peace','family','friends','awesome','perseverance','funny']
+
+def pre_process(text):
+  print(text)
+  text = text.translate(translator)
+  tokens = [word.lower() for word in nltk.word_tokenize(text)]
+  stop_words = stopwords.words("english")
+  no_stop_tokens = [word for word in tokens if word not in stop_words]
+  lemmas = [wordnet_lemmatizer.lemmatize(word) for word in no_stop_tokens]
+  return lemmas
+
+def count_songs():
+  with open('./new_songs.json') as data_file:
+    songs = json.load(data_file)
+    songs_tag_counter = Counter()
+    for song in songs:
+      tags = [tag.lower() for tag in song['tags']]
+      for sentiment in T:
+        for tag in tags:
+          if tag in T[sentiment]:
+            songs_tag_counter.update([sentiment])
+
+    print(songs_tag_counter.most_common(10))
+
+def create_train():
+  with open('./new_songs.json') as data_file:
+    songs = json.load(data_file)
+    with open("data/pre_train.csv", "w") as pre_train_file:
+      pre_train_writer = csv.writer(pre_train_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+      for song in songs:
+        lyric = pre_process(" ".join(str(element) for element in song['lyric']))
+        lyric = " ".join(str(element) for element in lyric)
+        tags = [tag.lower() for tag in song['tags']]
+        for sentiment in T:
+          for tag in tags:
+            if tag in T[sentiment]:
+              pre_train_writer.writerow([
+                lyric,
+                sentiment
+                ])
+
+def main():
+  create_train()
+
+if __name__ == '__main__':
+  main()
---
+++ train.py
@@ -0,0 +1,45 @@
+import numpy as np
+from sklearn.svm import SVC
+import pandas as pd
+from sklearn.grid_search import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def create_train():
+  df = pd.read_csv('data_lemma_lyrics_jl.csv')
+  X = df.iloc[:, :-1]
+  y = df.iloc[:, -1]
+  X = X.values
+  X = [element[0] for element in X]
+  print(X)
+  sentiment_encoder = LabelEncoder()
+  y = sentiment_encoder.fit_transform(y)
+  print(y.shape)
+  tfidf = TfidfVectorizer()
+
+  X = tfidf.fit_transform(X)
+  print(X.shape)
+  X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=1)
+
+  svm_model = SVC()
+
+  search_parameters = {
+      'degree': [2, 3, 4], 'gamma': (np.arange(0.1, 1.1, 0.1)), 'C': (np.arange(0.1, 1.1, 0.1)), 'kernel': ['poly']
+  }
+  model_svm = GridSearchCV(
+      svm_model, search_parameters, cv=10, scoring='accuracy', n_jobs=8)
+  model_svm.fit(X_train, y_train)
+  print(model_svm.best_score_)
+  print(model_svm.best_params_)
+  y_pred = model_svm.predict(X_test)
+  print(metrics.accuracy_score(y_pred, y_test))
+  print("model trained")
+
+def main():
+  create_train()
+
+if __name__ == '__main__':
+  main()
